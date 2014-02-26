7a9c9e067b95982860742d53e22caffb51023472
gogamoga <gogamoga@gmail.com>
2014-02-26 18:50:05 +0200

    Update validateContact.js

--- app/collection/validateContact.js
+++ app/collection/validateContact.js
@@ -1,25 +1,25 @@
-if typeof define == 'function' and define.amd
-  _define = define
-else
-  _define = (factory) -> module.exports = factory require
+define(function() {
 
-_define ->
-
-	###
+	/**
 	 * Validate a contact
-	###
-	return validateContact = (contact) -> 
-		result = valid: true, errors: [] 
+	 */
+	return function validateContact(contact) {
+		var valid, result;
+		result = { valid: true, errors: [] };
 
 		valid = contact && 'firstName' in contact && contact.firstName.trim();
-		if !valid 
-			result.valid = false
-			result.errors.push property: 'firstName', message: 'missing' 
-	
+		if(!valid) {
+			result.valid = false;
+			result.errors.push({ property: 'firstName', message: 'missing' });
+		}
 
 		valid = contact && 'lastName' in contact && contact.lastName.trim();
-		if !valid
-			result.valid = false
-			result.errors.push property: 'lastName', message: 'missing' 
+		if(!valid) {
+			result.valid = false;
+			result.errors.push({ property: 'lastName', message: 'missing' });
+		}
 
 		return result;
+	}
+
+});
